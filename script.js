5cf4c5c3eb390598f7adf72fddf9c071fe9f1897
Alejandro Soler <32654790+Asoler19@users.noreply.github.com>
2019-01-17 12:39:01 -0500

    Update script.js

--- script.js
+++ script.js
@@ -25,7 +25,7 @@ $(document).ready(function(){
         } 
     }); 
     $('#scroll').click(function(){ 
-        $("html, body").animate({ scrollTop: 0 }, 600); 
+        $("html, body").animate({ scrollTop: 0 }, 300); 
         return false; 
     }); 
 });
